fd65dd25fd5bb25ec4091a32275bcf5e95ad6c6d
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2024-08-16 20:34:30 -0300

    feat: Update HeroLiveNow component with shadow and background color changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,6 +14,21 @@ export default {
 				atomic: ["Atomic Marker", "cursive"],
 				"atomic-extras": ["Atomic Marker Extras", "cursive"],
 			},
+			colors: {
+				'electric-violet': {
+					'50': '#f6f2ff',
+					'100': '#eee8ff',
+					'200': '#dfd4ff',
+					'300': '#cab1ff',
+					'400': '#b085ff',
+					'500': '#9146ff',
+					'600': '#8d30f7',
+					'700': '#7f1ee3',
+					'800': '#6a18bf',
+					'900': '#57169c',
+					'950': '#370b6a',
+				},
+			}
 		},
 	},
 	plugins: [
