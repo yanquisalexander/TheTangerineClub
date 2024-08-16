4c5fcf7a482d0e830e79d5f4258c8c5831f0f1a9
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2024-08-16 17:30:01 -0300

    feat: Update Twitch stream API endpoint to include stream details

--- src/pages/api/stream.ts
+++ src/pages/api/stream.ts
@@ -4,12 +4,10 @@ import twitch from 'twitch-m3u8';
 
 export const GET = async ({ request }: APIContext) => {
     try {
-        const stream = await twitch.getStream("thetangerineclub");
+        const stream = await twitch.getStream("thetangerineclub", true);
+        console.log(stream);
 
-        const stream720p = stream.find((s: any) => s.resolution === '1280x720');
-        const m3u8Response = await fetch(stream720p.url);
-
-        return new Response(m3u8Response.body, {
+        return new Response(stream, {
             headers: {
                 'Content-Type': 'application/vnd.apple.mpegurl',
                 'Access-Control-Allow-Origin': '*',
