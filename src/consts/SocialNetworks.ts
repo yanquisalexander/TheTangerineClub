3e90787c83a09887ec1a49b457eb49b9c23b1271
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2024-08-15 21:25:08 -0300

    feat: Agregar iconos de redes sociales en el pie de página

---
+++ src/consts/SocialNetworks.ts
@@ -0,0 +1,32 @@
+import TwitchIcon from "@/icons/Twitch.astro";
+import InstagramIcon from "@/icons/Instagram.astro";
+import SpotifyIcon from "@/icons/Spotify.astro";
+import YouTubeIcon from "@/icons/YouTube.astro";
+import PatreonIcon from "@/icons/Patreon.astro";
+export const SOCIAL_NETWORKS = [
+    {
+        name: "Twitch",
+        url: "https://twitch.tv/TheTangerineClub",
+        icon: TwitchIcon,
+    },
+    {
+        name: "Instagram",
+        url: "https://instagram.com/TheTangerineClub",
+        icon: InstagramIcon,
+    },
+    {
+        name: "Spotify",
+        url: "https://open.spotify.com/artist/4NJhBhUqaOIQmuDsPEMrJL",
+        icon: SpotifyIcon,
+    },
+    {
+        name: "YouTube",
+        url: "https://youtube.com/TheTangerineClub",
+        icon: YouTubeIcon,
+    },
+    {
+        name: "Patreon",
+        url: "https://patreon.com/TheTangerineClub",
+        icon: PatreonIcon,
+    },
+];
