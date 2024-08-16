c0fa67ca13d11b1de554b6f471439bb2f5a7c29f
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2024-08-16 14:26:21 -0300

    feat: Actualizar fechas de lanzamiento de canciones en Music.ts
    Este commit actualiza las fechas de lanzamiento de las canciones en el archivo Music.ts. Se utiliza una función convertDate para convertir las fechas en formato día, mes, año a formato ISO. Esto mejora la legibilidad y mantenibilidad del código.

--- src/consts/Music.ts
+++ src/consts/Music.ts
@@ -1,15 +1,22 @@
 import type { Music } from "@/types/Music";
 
+const convertDate = (day: number, month: number, year: number) => new Date(year, month - 1, day).toISOString();
+
 export const MUSICS: Music[] = [
     {
         id: 'flowers-of-september',
         title: 'Flowers of September',
-        releaseDate: new Date(2020, 2, 1).toISOString(),
+        releaseDate: convertDate(1, 3, 2020),
         hasLyrics: true,
     },
     {
         id: 'disarm',
         title: 'Disarm',
-        releaseDate: new Date(2020, 9, 10).toISOString(),
-    }
+        releaseDate: convertDate(10, 10, 2020),
+    },
+    {
+        id: 'seasons',
+        title: 'Seasons',
+        releaseDate: convertDate(14, 2, 2021),
+    },
 ]
