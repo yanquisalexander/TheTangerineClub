495bc967c568e91c56db19b8120ddeaf2da4a556
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2024-08-15 21:19:56 -0300

    feat: Agregar iconos de redes sociales en el pie de página
    Agregar iconos de redes sociales en el pie de página para enlazar a las cuentas de Twitch, Instagram, Spotify, YouTube y Patreon de TheTangerineClub.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 import auth from "auth-astro";
 
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(),
+  integrations: [tailwind(), react()
     //auth()
   ],
   output: "server",
