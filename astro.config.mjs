bc0fe90df87f34a88293497388b32619277e1c29
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2024-08-16 21:27:17 -0300

    feat: Add sitemap integration and update SEO component

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 import auth from "auth-astro";
 
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()
-    //auth()
-  ],
+  //auth()
+  , sitemap()],
   output: "server",
   adapter: vercel(),
   devToolbar: {
--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,15 +1,15 @@
-import type { APIContext } from "astro";
+import type { APIRoute } from 'astro';
 
-export const GET = async ({ request }: APIContext) => {
-    const url = new URL(request.url);
-    return new Response(`User-agent: *
+const robotsTxt = `
+User-agent: *
 Allow: /
+Sitemap: ${new URL('sitemap-index.xml', import.meta.env.SITE).href}
+`.trim();
 
-Host: ${url.hostname}
-
-        `, {
+export const GET: APIRoute = () => {
+    return new Response(robotsTxt, {
         headers: {
-            "Content-Type": "text/plain",
+            'Content-Type': 'text/plain; charset=utf-8',
         },
     });
-}
+};
