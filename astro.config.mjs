855c71070af6c06ac43da4d6698b88e306f2ddc4
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2024-08-18 13:47:07 -0300

    feat: Update Toaster component to use React

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import auth from "auth-astro";
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(),
+  react(),
     //auth()
     , sitemap(), preact()],
   output: "server",
