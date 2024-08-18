5705340dcdf0d22845ce039d2038246a9bd053a5
Alexander Barrios <alex.barrios2910@gmail.com>
2024-08-18 21:21:40 +0000

    dev: Add astro db integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,17 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 //import auth from "auth-astro";
 import sitemap from "@astrojs/sitemap";
-
 import preact from "@astrojs/preact";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(),
     //auth()
-    , sitemap(), preact({ compat: true })],
+  , sitemap(), preact({
+    compat: true
+  }), db()],
   output: "server",
   adapter: vercel(),
   devToolbar: {
