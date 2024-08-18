ee650e37a244e2dd79d0a7b66f2da8a39fb8bd12
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2024-08-18 13:50:34 -0300

    feat: Update Toaster component to use Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import auth from "auth-astro";
+//import auth from "auth-astro";
 import sitemap from "@astrojs/sitemap";
-import react from "@astrojs/react";
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(),
-  react(),
     //auth()
-    , sitemap(), preact()],
+    , sitemap(), preact({ compat: true })],
   output: "server",
   adapter: vercel(),
   devToolbar: {
