d1abef4948414bbe255cedca519e8d9f52f8f404
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2024-08-19 12:31:59 -0300

    fix: Javascript error on runtime

--- cypress/e2e/originals.cy.ts
+++ cypress/e2e/originals.cy.ts
@@ -7,3 +7,17 @@ it('have all music cards in the page (From MUSICS const)', () => {
         cy.get(`[data-music-id="${music.id}"]`).should('exist');
     });
 })
+
+it('render lyrics if music has lyrics', () => {
+    const musicsWithLyrics = MUSICS.filter((music) => music.hasLyrics);
+
+    if (!musicsWithLyrics) {
+        return;
+    }
+
+    for (let i = 0; i < musicsWithLyrics.length; i++) {
+        cy.visit(`/originals/${musicsWithLyrics[i].id}`);
+
+        cy.get('#lyrics-container').should('exist');
+    }
+})
