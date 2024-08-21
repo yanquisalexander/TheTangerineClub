365d808c5f120c0d7ab3c292a4bcc51962543250
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2024-08-21 13:13:34 -0300

    chore: Update Astro config to include web-vitals integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,17 @@ import vercel from "@astrojs/vercel/serverless";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
 import db from "@astrojs/db";
-
 import auth from "auth-astro";
+
+import webVitals from "@astrojs/web-vitals";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    sitemap(),
-    preact({
-      compat: true
-    }),
-    db(),
-    auth({
-      configFile: './auth.config.mjs'
-    })
-  ],
+  integrations: [tailwind(), sitemap(), preact({
+    compat: true
+  }), db(), auth({
+    configFile: './auth.config.mjs'
+  }), webVitals()],
   output: "server",
   adapter: vercel(),
   devToolbar: {
