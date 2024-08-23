0d4c0552312acebb56472eccd64bedf24bbb56c7
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2024-08-23 10:27:43 -0300

    chore: Update Cypress configuration with increased task and command timeouts

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,5 +3,7 @@ export default defineConfig({
     e2e: {
         supportFile: false,
         baseUrl: 'http://localhost:4321',
+        taskTimeout: 10000,
+        defaultCommandTimeout: 10000
     }
 })
